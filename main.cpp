f9d6e778b0648e607c2792c2776e582272fceb91
Timoty <timoty.granziero@gmail.com>
2017-10-26 12:34:49 +0200

    Aggiunta la classe Bolletta

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
-#include "telefonata.h"
+#include "bolletta.h"
 #include <iostream>
 
 using std::cout;
 using std::endl;
 
 int main(){
-    Telefonata a(Orario(5,34,12), Orario(5,39,00), PhoneNumber("3470101011"));
-    cout << a << endl;
+    Telefonata t1(Orario(14,15), Orario(14,19), PhoneNumber("0444838383"));
+    Telefonata t2(Orario(15,15), Orario(15,24), PhoneNumber("0444838383"));
+    Telefonata t3(Orario(16,2), Orario(17), PhoneNumber());
+    Bolletta b1, b2;
+    b1.aggiungiTelefonata(t1);
+    b1.aggiungiTelefonata(t2);
+    b1.aggiungiTelefonata(t3);
+
+    b2 = b1;
+    b1.togliTelefonata(t2);
+
+    cout << b1 << endl << b2;
     return 0;
 }
